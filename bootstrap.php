e10297bb0d306fe779b39fd7f06f8ec202afd8c8
echo511 <ntsiongas@gmail.com>
2013-08-19 10:05:00 +0200

    Formatted bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -4,16 +4,14 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Config\Configurator;
 
-// $configurator->setDebugMode(TRUE);
 $configurator->enableDebugger(__DIR__ . '/../log');
-
 $configurator->setTempDirectory(__DIR__ . '/../temp');
+
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__)
 	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/parameters.neon');
-$container = $configurator->createContainer();
 
-return $container;
+return $configurator->createContainer();
